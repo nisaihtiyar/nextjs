7c6eecd237784279897f0f5daf8a02cabb9d020b
nisaihtiyar <56438271+nisaihtiyar@users.noreply.github.com>
2023-12-25 18:33:54 +0300

    tailwind css entegre edildi

--- my-app/src/app/page.jsx
+++ my-app/src/app/page.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      page
+       <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
     </div>
   )
 }
